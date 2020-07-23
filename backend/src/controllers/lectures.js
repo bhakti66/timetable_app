7927b920e8d8bc2976f4c43714527a4e6bd8158c
Bhakti Gajjar <bhakti.gajjar@arastusystems.com>
2020-07-23 17:27:23 +0530

    Add lecture API

--- backend/src/controllers/lectures.js
+++ backend/src/controllers/lectures.js
@@ -1,10 +1,9 @@
 const status = require('http-status');
 
 const lectureModel = require('../models/lectures');
-
-
 const has = require('has-keys');
-
+const moment = require('moment')
+const { Op } = require('sequelize')
 
 module.exports = {
     async getLectures(req, res){
@@ -16,17 +15,56 @@ module.exports = {
         if(!has(req.body, ['sub_id', 'class_id', 'prof_id', 'lec_date', 'lec_start_time']))
             throw {code: status.BAD_REQUEST, message: 'You must specify the lecture details'};
 
-        //prof hours/day and hours/week check
-        lectureModel.findAndCountAll({where: {prof_id: req.body.prof_id, lec_date: req.body.lec_date}}).then((profLecs)=>{
-            //lectures per day check
-            if(profLecs.count<4){
-                
+        //check if prof and class is available on the specified date n time
+        lectureModel.findAndCountAll({where: {prof_id: req.body.prof_id, class_id: req.body.class_id, lec_date: req.body.lec_date,lec_start_time:req.body.lec_start_time}}).then((clashes)=>{
+            if(clashes.count>0){
+                res.json({status: false, message: 'Lecture clashes with other lectures'});
             }
-        })  
-
-        
-        //classes hours/day and hours/week check
-        
-        res.json({status: true, message: 'Lecture Added'});
+            else{
+                mDate = moment(new Date(req.body.lec_date))
+                startOfWeek = mDate.startOf('isoWeek')
+                startOfWeek = startOfWeek.year()+"/"+(startOfWeek.month()+1)+"/"+startOfWeek.date()
+                endOfWeek = mDate.endOf('week').subtract(1,'days')
+                endOfWeek = endOfWeek.year()+"/"+(endOfWeek.month()+1)+"/"+endOfWeek.date()
+                lectureModel.findAndCountAll({where: {prof_id: req.body.prof_id, lec_date:{
+                    [Op.gte]: startOfWeek,
+                    [Op.lte]: endOfWeek
+                }},group: ['lec_date']}).then((profLecs)=>{
+                    profWeeklyHours = 0
+                    profDailyHours = 0
+                    profLecs.count.map((ele)=>{
+                        profWeeklyHours += ele.count
+                        if(ele.lec_date == req.body.lec_date)
+                            profDailyHours = ele.count 
+                    })
+                    //lectures per day check for professor
+                    if(profDailyHours<4 && profWeeklyHours<18){
+                        lectureModel.findAndCountAll({where: {class_id: req.body.class_id, lec_date:{
+                            [Op.gte]: startOfWeek,
+                            [Op.lte]: endOfWeek
+                        }},group: ['lec_date']}).then((classLecs)=>{
+                            classWeeklyHours = 0
+                            classDailyHours = 0
+                            classLecs.count.map((ele)=>{
+                                classWeeklyHours += ele.count
+                                if(ele.lec_date == req.body.lec_date)
+                                    classDailyHours = ele.count
+                            })
+                            //lectures per day check for class
+                            if(classDailyHours<6 && classWeeklyHours<25){
+                                let { sub_id, class_id, prof_id, lec_date, lec_start_time } = req.body
+                                lectureModel.create({ sub_id, class_id, prof_id, lec_date, lec_start_time }).then(()=>{
+                                    res.json({status: true, message: 'Lecture Added'});
+                                });
+                            }else{
+                                res.json({status: true, message: 'Students cannot have more than 6 lectures a day or 25 lectures a week'});
+                            }
+                        });
+                    }else{
+                        res.json({status: true, message: 'Professor cannot take more than 4 lectures a day or 18 lectures a week'});
+                    }
+                })  
+            }
+        });
     },
 }
